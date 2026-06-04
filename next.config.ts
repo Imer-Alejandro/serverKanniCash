84f135fdc0e1f151b35d8e8c3ac05fa34d1bbf27
imer alejandro <consymer1@gmail.com>
2026-06-04 18:15:59 -0400

    cambios de compilacion

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
-  // @ts-ignore
+  // @ts-expect-error
   turbopack: {
     root: '.',
   },
